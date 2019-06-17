515427ccc0a755f4c9fb7c21a59ab54745f6c0dc
vadimgs <vadim.didenko@greg-solutions.com>
2019-06-17 17:20:10 +0300

    Add policy and ecr modules

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -5,3 +5,15 @@ output "endpoint" {
 output "kubeconfig-certificate-authority-data" {
   value = "${aws_eks_cluster.eks_cluster.certificate_authority.0.data}"
 }
+
+output "eks_cluster_id" {
+  value = "${aws_eks_cluster.eks_cluster.id}"
+}
+
+output "eks_cluster_arn" {
+  value = "${aws_eks_cluster.eks_cluster.arn}"
+}
+
+output "eks_cluster_name" {
+  value = "${aws_eks_cluster.eks_cluster.name}"
+}
